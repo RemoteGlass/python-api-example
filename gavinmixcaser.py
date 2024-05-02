f9ecd21213abccd859599b90bec084df91095aca
RemoteGlass <90830482+RemoteGlass@users.noreply.github.com>
2024-05-02 19:24:46 +0700

    updatev2
    deleting app copy
    delet mixcaser

--- gavinmixcaser.py
+++ gavinmixcaser.py
@@ -40,75 +40,8 @@ class GavinMixcaser(Resource):
       
     
     
-class ProcessText(Resource):
-    def get(self):
-        """
-        This method responds to the GET request for processing text and returns the processed text.
-        ---
-        tags:
-        - Text Processing
-        parameters:
-            - name: text
-              in: query
-              type: string
-              required: true
-              description: The text to be processed
-            - name: duplication_factor
-              in: query
-              type: integer
-              required: false
-              description: The number of times to duplicate the text
-            - name: capitalization
-              in: query
-              type: string
-              required: false
-              enum: [UPPER, LOWER,MIXED,UPPER LOWER, None]
-              description: The capitalization style for the text
-        responses:
-            200:
-                description: A successful GET request
-                content:
-                    application/json:
-                      schema:
-                        type: object
-                        properties:
-                            processed_text:
-                                type: string
-                                description: The processed text
-        """
-        text = request.args.get('text')
-        duplication_factor = int(request.args.get('duplication_factor', 1))
-        capitalization = request.args.get('capitalization', 'None')
-        def mixedtextchecker(){
-            if text == text.upper(){
-                text.upper()
-            }
-            if text == text.lower(){
-                text.lower()
-            }
 
-        }
-        # Validate capitalization input
-        if capitalization not in ['UPPER', 'LOWER', 'UPPER LOWER', 'MIXED', 'None']:
-            return {"error": "Invalid capitalization value"}, 400
 
-        # Process the text based on duplication_factor and capitalization
-        if capitalization == 'UPPER':
-            text = text.upper()
-        elif capitalization == 'LOWER':
-            text = text.lower()
-        elif capitalization == 'MIXED':
-            mixedtextchecker()
-        elif capitalization == 'UPPER LOWER':
-            text = text.lower()
-            text = text.lower()
-            mixedtext = text.lower() + " " + text.upper()
-    
-        processed_text = text * duplication_factor
-
-        return {"processed_text": processed_text}, 200
-
-api.add_resource(ProcessText, "/process_text")
 api.add_resource(GavinMixcaser, "/mixcase")
 
 if __name__ == "__main__":
